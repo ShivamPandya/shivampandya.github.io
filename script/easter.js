489d95b7d8f32a75493f19cf84925ca7b151e4f7
Shivam Pandya <sampandya.sp@gmail.com>
2019-03-27 02:04:47 +0530

    resume pushed

---
+++ script/easter.js
@@ -0,0 +1,6 @@
+particlesJS.load('particles-js',
+    'particles.json', function(){
+      console.log("Well, welcome to the site (as well as to the console :D )");
+    });
+
+console.log("Did you notice that 1000 digits of Pi? I am big fan of Pi, that's why it is there!")
